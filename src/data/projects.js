8eb2f1103c653fda276210d25c431314891a3c2f
Nishant Rai <rainishant4886@gmail.com>
2026-08-04 09:31:50 +0530

    Updated project description and features

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,10 +3,19 @@ const projects = [
     id: 1,
     title: "AI Code Plagiarism Detector",
     description:
-      "AI-powered web application that detects code similarity with real-time plagiarism percentage and streaming responses.",
-    tech: ["React", "Node.js", "Express", "MongoDB"],
-    github: "https://github.com/rainishantt/ai-code-plagiarism--det",
-    demo: "https://ai-code-plagiarism-det.vercel.app/",
+      "AI-powered code similarity analyzer using the Groq API and Llama 3.1  Instant to generate real-time similarity scores and AI-driven explanations",
+    tech: [
+      "React.js",
+      "Node.js",
+      "Express.js",
+      "JavaScript",
+      "Groq API",
+      "Llama 3.1  Instant",
+      "REST API",
+      "Vercel"
+    ],
+    github: "https://github.com/rainishantt/ai-code-plagiarism-detector-f",
+    demo: "https://ai-code-plagiarism-detector-f-cigg.vercel.app/",
     image: "/projects/project1.png",
   },
   {
@@ -14,9 +23,9 @@ const projects = [
     title: "AI Portfolio Website",
     description:
       "Responsive portfolio built using React with modern UI, animations, and optimized performance.",
-    tech: ["React", "Tailwind CSS", "JavaScript"],
-    github: "https://github.com/rainishantt/portfolio",
-    demo: "https://portfolio-omega-pink-67.vercel.app/",
+    tech: ["React", "Tailwind CSS", "JavaScript","Github","Vercel"],
+    github: "https://github.com/rainishantt/Portfolio-website",
+    demo: "https://pportfolio-website.vercel.app/",
     image: "/projects/project2.png",
   },
 ];
